9abfbb6385f7b43c7265cbaf10553087b35d52c2
Sam Carlson <sam.p.carlson@gmail.com>
2019-01-04 22:38:41 -0600

    vectorize resp distribution sampling

--- makeModelParameters.R
+++ makeModelParameters.R
@@ -1,1 +1,36 @@
+sampleRespFromProbs=function(denseJamProbs,minER=50){
+  source('~/R/projects/mapRespAndLogs/dataByBatch.R')
+  #define metabModel
+  metabData=dataByBatch(3,excludeDataTypeIDXs = c(37,58))
+  metabModel=lm(-ER~mean_jamsPerKm,data=metabData)
+  
+  #denseJam distribution
+  jamDist=dataByBatch(6)[,c("jamsPerKm")]
+  jamDist=jamDist[complete.cases(jamDist)]
+  denseJamDist=jamDist[jamDist>5]
+  sparseJamDist=jamDist[jamDist<5]
+  
+  sampleRespFromProb=function(denseJamProb,min=minER){
+    if(denseJamProb>runif(1)){
+      #dense jams are here - sample from jam dist & calc resp
+      jamDensity=base::sample(denseJamDist,1)
+    } else {
+      #dense jams are not here - sample around sparse jam dist
+      jamDensity=base::sample(sparseJamDist,1)
+    }
+    
+    ER=max(min,rnorm(1,mean=predict(metabModel,newdata = data.frame(mean_jamsPerKm=jamDensity)),
+                       sd=sigma(metabModel)))
+    return(ER)
+  }
+  
+  ERs=sapply(denseJamProbs,sampleRespFromProb)
+  
+  return(ERs)
+}
+
 segData=read.csv("segs_width_jamHabitat.csv")[,c("locationIDX","denseJamProbability","width")]
+
+segData$resp=sampleRespFromProbs(segData$denseJamProbability)
+#fucking fuck units are wrong - need to know neo model units
+
